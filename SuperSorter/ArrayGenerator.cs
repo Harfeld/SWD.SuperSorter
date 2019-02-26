8b245c709d68d9a4a431ad99fef7c6603ac8cd63
Andreas <201608930@post.au.dk>
2019-02-26 11:28:06 +0100

    Stor forvirring.. Stadig i gang med opgave 1

--- SuperSorter/ArrayGenerator.cs
+++ SuperSorter/ArrayGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,21 @@ namespace SuperSorter
 {
     class ArrayGenerator
     {
+       private Random rand;
+
+       public int[] Generate(int size, int max, int seed)
+       {
+            int[] array = new int[size];
+            Fill(array, max, seed);
+            return array;
+       }
+       private Fill(int[] ar, int max, int seed)
+       {
+           rand = new Random(seed);
+           foreach (var number in ar)
+           {
+               ar[number] = rand.Next(max);
+           }
+        }
     }
 }
